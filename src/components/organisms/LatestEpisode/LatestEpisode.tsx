695987d4e3b8cd273cefce347227e72f511b4e27
Rafael Batista <rafaabatistas@gmail.com>
2021-06-12 21:47:11 -0300

    Component of last released cards

---
+++ src/components/organisms/LatestEpisode/LatestEpisode.tsx
@@ -0,0 +1,32 @@
+import styles from './styles.module.scss';
+
+import { useContext } from 'react';
+import { DescriptionLatestEpisode } from '../../atoms/DescriptionLatestEpisode/DescriptionLatestEpisode';
+import { TitleHome } from '../../atoms/TitleHome/TitleHome';
+import { Card } from '../../molecules/Card/Card';
+import { LatestEpisodeContext } from '../../../contexts/EpisodeContext';
+
+export const LatestEpisode = () => {
+  const { latestEpisodeList } = useContext(LatestEpisodeContext);
+
+  return (
+    <>
+      <TitleHome>Últimos Laçamentos</TitleHome>
+      <section className={styles.card}>
+        {latestEpisodeList.map(episode => {
+          return (
+            <Card thumbnail={episode.thumbnail} title={episode.title}>
+              <DescriptionLatestEpisode
+                id={episode.id}
+                title={episode.title}
+                members={episode.members}
+                publishedAt={episode.publishedAt}
+                durationAsString={episode.durationAsString}
+              />
+            </Card>
+          );
+        })}
+      </section>
+    </>
+  );
+};
