f999c339d66f614749081aa72c024c554400b627
Rafael Batista <rafaabatistas@gmail.com>
2021-06-12 21:46:27 -0300

    Component that contains the episode listing part

---
+++ src/components/organisms/AllEpisode/AllEpisode.tsx
@@ -0,0 +1,34 @@
+import styles from './styles.module.scss';
+
+import { useContext } from 'react';
+import { TitleHome } from '../../atoms/TitleHome/TitleHome';
+import { TheadHome } from '../../molecules/TheadHome/TheadHome';
+import { RowsTableHome } from '../../molecules/RowsTableHome/RowsTableHome';
+import { AllEpisodeContext } from '../../../contexts/AllEpisodeContext';
+
+export const AllEpisode = () => {
+  const { allEpisodeList } = useContext(AllEpisodeContext);
+
+  return (
+    <section className={styles.table_details_episodes}>
+      <TitleHome>Todos episódios</TitleHome>
+      <table cellSpacing={0} className={styles.table_details}>
+        <TheadHome />
+        <tbody>
+          {allEpisodeList.map(episode => {
+            return (
+              <RowsTableHome
+                id={episode.id}
+                thumbnail={episode.thumbnail}
+                title={episode.title}
+                members={episode.members}
+                publishedAt={episode.publishedAt}
+                durationAsString={episode.durationAsString}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </section>
+  );
+};
