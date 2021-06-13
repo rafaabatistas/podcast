9f03abd5d9f6c2877827f26bd499a5eb3c284ee9
Rafael Batista <rafaabatistas@gmail.com>
2021-06-12 21:42:21 -0300

    Identifying the Header

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -2,11 +2,10 @@ import styles from './styles.module.scss';
 
 import { NavBar } from '../../molecules/NavBar/NavBar';
 
-
 export const Header = () => {
   return (
     <header className={styles.headerContainer}>
       <NavBar />
     </header>
   );
-}
+};
