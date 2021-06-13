ef3f3c4819d3be87d3b0c08a92e012cabaaba21c
Rafael Batista <rafaabatistas@gmail.com>
2021-06-12 22:01:04 -0300

    Inserting the context in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,25 @@ import '../styles/global.scss';
 
 import { Header } from '../components/organisms/Header/Header';
 import { Player } from '../components/organisms/Player/Player';
+import ContextProviders from '../contexts/contextUtils/providers';
 
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={styles.wrapper}>
-      <head>
-      <title>Podcastr</title>
-      </head>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
-    )
+    <ContextProviders>
+      <div className={styles.wrapper}>
+        <head>
+          <title>Podcastr</title>
+        </head>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </ContextProviders>
+  );
 }
 
-export default MyApp
+export default MyApp;
