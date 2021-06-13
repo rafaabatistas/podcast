d51ef0a182c2eca14756ad535e4ba6d7ee2e631a
Rafael Batista <rafaabatistas@gmail.com>
2021-06-12 21:22:53 -0300

    Add folder for typing

---
+++ src/types/Episode.ts
@@ -0,0 +1,11 @@
+export type Episode = {
+  id?: string;
+  url?: string;
+  title?: string;
+  members?: string;
+  duration?: string;
+  thumbnail?: string;
+  publishedAt?: string;
+  description?: string;
+  durationAsString?: string;
+}
