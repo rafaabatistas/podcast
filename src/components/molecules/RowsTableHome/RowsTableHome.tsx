eb468bb017d518135e2dc2654aaed6fd7c14438c
Rafael Batista <rafaabatistas@gmail.com>
2021-06-12 21:44:51 -0300

    Creating the table rows showing episode information

---
+++ src/components/molecules/RowsTableHome/RowsTableHome.tsx
@@ -0,0 +1,43 @@
+import styles from './styles.module.scss';
+
+import type { Episode } from '../../../types/Episode';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const RowsTableHome = ({
+  id,
+  thumbnail,
+  title,
+  members,
+  publishedAt,
+  durationAsString,
+}: Episode) => {
+  return (
+    <tr className={styles.episode_details}>
+      <td>
+        <Image
+          width={120}
+          height={120}
+          src={thumbnail}
+          alt={title}
+          objectFit="cover"
+          className={styles.image}
+        />
+      </td>
+      <td>
+        <Link href={`/episodes/${id}`}>
+          <a className={styles.title}>{title}</a>
+        </Link>
+      </td>
+      <td className={styles.memberEpisode}>{members}</td>
+      <td className={styles.dataEpisode}>{publishedAt}</td>
+      <td>{durationAsString}</td>
+      <td>
+        <button type="button" className={styles.play_button}>
+          <img src="/play-green.svg" alt="Tocar Episodio" />
+        </button>
+      </td>
+    </tr>
+  );
+};
