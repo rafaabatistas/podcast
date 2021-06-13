41b47882d09836eee12d217d0f7fdf85eb7cbc60
Rafael Batista <rafaabatistas@gmail.com>
2021-06-12 21:52:23 -0300

    Contexts of all episodes

---
+++ src/contexts/AllEpisodeContext.tsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react';
+
+export const AllEpisodeContext = createContext<any | null>(null);
+
+import type { EpisodeContextProps } from './EpisodeContext';
+
+const AllEpisodeContextProviders = ({children}: EpisodeContextProps) => {
+  const [allEpisodeList, setAllepisodeList] = useState([]);
+  return (
+    <AllEpisodeContext.Provider value={{ allEpisodeList, setAllepisodeList }}>
+      {children}
+    </AllEpisodeContext.Provider>
+  );
+};
+
+export  default AllEpisodeContextProviders;
